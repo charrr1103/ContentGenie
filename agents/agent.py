d2ae697d74cc21457bf7864654e89fd4be3afbd4
charm <charmainehooi.1119@gmail.com>
2025-06-06 13:47:48 +0800

    Added __init__.py and .env

--- agents/agent.py
+++ agents/agent.py
@@ -2,12 +2,12 @@ from google.adk.agents import LlmAgent
 from google.adk.tools.agent_tool import AgentTool
 
 from . import prompt 
-from .sub_agents.audience_analyst import audience_analyst_agent
-from .sub_agents.content_strategist import content_strategist_agent
-from .sub_agents.copywriter import copywriter_agent
-from .sub_agents.formatter import formatter_agent
-from .sub_agents.reviewer import reviewer_agent
-from .sub_agents.scheduler import scheduler_agent
+from .subagents.audience_analyst_agent import audience_analyst_agent
+from .subagents.content_strategis_agent import content_strategist_agent
+from .subagents.copywriter_agent import copywriter_agent
+from .subagents.formatter_agent import formatter_agent
+from .subagents.reviewer_agent import reviewer_agent
+from .subagents.scheduler_agent import scheduler_agent
 
 MODEL = "gemini-2.5-pro-preview-05-06"
 
